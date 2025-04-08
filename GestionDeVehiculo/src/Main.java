19a75135339d576ed94d371ebbb7c06495378e1a
Emicora <e.coratolo@alumno.um.edu.ar>
2025-04-08 20:23:03 -0300

    validar interfaz

--- GestionDeVehiculo/src/Main.java
+++ GestionDeVehiculo/src/Main.java
@@ -3,15 +3,12 @@ public class Main {
 
         // Crear tres vehículos genéricos
         Vehiculo vehiculo1 = new Vehiculo("ABC123", "Fiat", 2010, 1500);
-        Vehiculo vehiculo2 = new Vehiculo("DEF456", "Ford", 2012, 1600);
-        Vehiculo vehiculo3 = new Vehiculo("GHI789", "Chevrolet", 2015, 2000);
+        Vehiculo auto1     = new Auto("MNO345", "Honda", 2021, 800, 5);
         Camion camion1    = new Camion("JKL012", "Scania", 2018, 5000, true);
 
         VehiculoPrinter.imprimir(vehiculo1);
         System.out.println("----------------");
-        VehiculoPrinter.imprimir(vehiculo2);
-        System.out.println("----------------");
-        VehiculoPrinter.imprimir(vehiculo3);
+        VehiculoPrinter.imprimir(auto1);
         System.out.println("----------------");
         VehiculoPrinter.imprimir(camion1);
     }
