caeecf0d66012bd6590f4038e8e378a38207b23d
Emicora <e.coratolo@alumno.um.edu.ar>
2025-04-08 20:09:47 -0300

    modificar vehiculoPrinter polimorfismo

--- GestionDeVehiculo/src/Main.java
+++ GestionDeVehiculo/src/Main.java
@@ -5,11 +5,14 @@ public class Main {
         Vehiculo vehiculo1 = new Vehiculo("ABC123", "Fiat", 2010, 1500);
         Vehiculo vehiculo2 = new Vehiculo("DEF456", "Ford", 2012, 1600);
         Vehiculo vehiculo3 = new Vehiculo("GHI789", "Chevrolet", 2015, 2000);
+        Camion camion1    = new Camion("JKL012", "Scania", 2018, 5000, true);
 
         VehiculoPrinter.imprimir(vehiculo1);
         System.out.println("----------------");
         VehiculoPrinter.imprimir(vehiculo2);
         System.out.println("----------------");
         VehiculoPrinter.imprimir(vehiculo3);
+        System.out.println("----------------");
+        VehiculoPrinter.imprimir(camion1);
     }
 }
