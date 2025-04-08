585294564ec4ca320a6655a198f78f700d3e8ff6
Emicora <e.coratolo@alumno.um.edu.ar>
2025-04-08 20:36:11 -0300

    arraylist vehiculo

--- GestionDeVehiculo/src/Main.java
+++ GestionDeVehiculo/src/Main.java
@@ -1,15 +1,17 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
+        List<Vehiculo> vehiculos = new ArrayList<>();
+        vehiculos.add(new Vehiculo("ABC123", "Fiat", 2010, 1500));
+        vehiculos.add(new Camion("JKL012", "Scania", 2018, 5000, true));
+        vehiculos.add(new Auto("MNO345", "Honda", 2021, 800, 5));
 
-        // Crear tres vehículos genéricos
-        Vehiculo vehiculo1 = new Vehiculo("ABC123", "Fiat", 2010, 1500);
-        Vehiculo auto1     = new Auto("MNO345", "Honda", 2021, 800, 5);
-        Camion camion1    = new Camion("JKL012", "Scania", 2018, 5000, true);
-
-        VehiculoPrinter.imprimir(vehiculo1);
-        System.out.println("----------------");
-        VehiculoPrinter.imprimir(auto1);
-        System.out.println("----------------");
-        VehiculoPrinter.imprimir(camion1);
+        // Se recorre la lista e imprime cada vehículo.
+        for (Vehiculo v : vehiculos) {
+            VehiculoPrinter.imprimir(v);
+            System.out.println("----------------");
+        }
     }
-}
+} 
